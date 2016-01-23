c7779c1dca186eba2699436fa20e146812b5ce76
Michael Ries <michael@riesd.com>
2016-01-22 23:10:44 -0700

    basic addition operators

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,2 +1,10 @@
 defmodule Calculator do
+  @regex ~r{(\d+)\s*\+\s*(\d+)}
+
+  def evaluate(expression) do
+    matches = Regex.run(@regex, expression)
+    a = Enum.at(matches, 1)
+    b = Enum.at(matches, 2)
+    String.to_integer(a) + String.to_integer(b)
+  end
 end
--- test/calculator_test.exs
+++ test/calculator_test.exs
@@ -2,7 +2,11 @@ defmodule CalculatorTest do
   use ExUnit.Case
   doctest Calculator
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "calculate a basic addition expression`" do
+    assert Calculator.evaluate("1 + 1") == 2
+  end
+
+  test "calculate different addition" do
+    assert Calculator.evaluate("1+2") == 3
   end
 end
