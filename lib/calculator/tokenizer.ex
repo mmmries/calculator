a11166f5c2afa659ed171da72df6ef7027b42d49
Michael Ries <michael@riesd.com>
2016-01-23 20:38:50 -0700

    Enum.map + Enum.reject (is_nil)

--- lib/calculator/tokenizer.ex
+++ lib/calculator/tokenizer.ex
@@ -2,7 +2,7 @@ defmodule Calculator.Tokenizer do
   def tokenize(str) do
     chunks = String.split(str, ~r/\b/)
     tokens = Enum.map(chunks, fn(chunk) -> convert_chunk(chunk) end)
-    Enum.filter(tokens, fn(token) -> !is_nil(token) end)
+    Enum.reject(tokens, fn(token) -> is_nil(token) end)
   end
 
   def convert_chunk(""), do: nil
