a72d2756f1c72d1cf5ac6df39dbfee827c8003f9
Michael Ries <michael@riesd.com>
2016-01-23 20:24:58 -0700

    Tokenize multiple tokens, Enum.map + Enum.filter

--- lib/calculator/tokenizer.ex
+++ lib/calculator/tokenizer.ex
@@ -1,17 +1,12 @@
 defmodule Calculator.Tokenizer do
   def tokenize(str) do
     chunks = String.split(str, ~r/\b/)
-    convert_list(chunks)
+    tokens = Enum.map(chunks, fn(chunk) -> convert_chunk(chunk) end)
+    Enum.filter(tokens, fn(nil) -> false
+                          (token) -> true end)
   end
 
-  defp convert_list([]), do: []
-  defp convert_list(["" | rest]), do: convert_list(rest)
-  defp convert_list([chunk | rest]) do
-    converted = convert_chunk(chunk)
-    rest_converted = convert_list(rest)
-    [converted | rest_converted]
-  end
-
+  def convert_chunk(""), do: nil
   def convert_chunk("+"), do: {:operator, :plus}
   def convert_chunk(str), do: String.to_integer(str)
 end
